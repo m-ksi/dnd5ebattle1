62c8cf2eeecc8c42157c71d4ee363885a751250c
FeloniousS <maximtimof194@gmail.com>
2020-05-21 12:22:04 +0300

    making grid

--- mainloop.py
+++ mainloop.py
@@ -1,15 +1,10 @@
 import arcade
-import math
 import settings
 
-
-WIDTH = 20
-HEIGHT = 20
-MARGIN = 5
-ROW_COUNT = 10
-COLUMN_COUNT = 10
-SCREEN_WIDTH = WIDTH * COLUMN_COUNT + MARGIN * (COLUMN_COUNT + 1)
-SCREEN_HEIGHT = HEIGHT * ROW_COUNT + MARGIN * (ROW_COUNT + 1)
+SCREEN_WIDTH = settings.SCREEN_WIDTH
+SCREEN_HEIGHT = SCREEN_WIDTH // 16 * 9
+FIELD_WIDTH = SCREEN_WIDTH * 13 // 16
+FIELD_HEIGHT = SCREEN_HEIGHT * 7 // 9
 
 
 class MyGame(arcade.Window):
@@ -17,53 +12,30 @@ class MyGame(arcade.Window):
     Main application class.
     """
 
-    def __init__(self, width, height):
-        super().__init__(width, height)
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
 
-        self.grid = [[0 for x in range(10)] for y in range(10)]
-        self.grid[1][5] = 1
-
-        arcade.set_background_color(arcade.color.BLACK)
-
-        self.grid_shape_list = None
-        self.create_shapes_from_grid()
-
-    def create_shapes_from_grid(self):
-        self.grid_shape_list = arcade.ShapeElementList()
-        for row in range(ROW_COUNT):
-            for column in range(COLUMN_COUNT):
-                x = MARGIN + WIDTH / 2 + (WIDTH + MARGIN) * column
-                y = MARGIN + HEIGHT / 2 + (HEIGHT + MARGIN) * row
-                color = arcade.color.WHITE
-                if self.grid[row][column] == 1:
-                    color = arcade.color.GREEN
-                rect = arcade.create_rectangle_filled(x, y, WIDTH, HEIGHT, color)
-                self.grid_shape_list.append(rect)
+        arcade.set_background_color(arcade.color.DARK_BROWN)
 
     def on_draw(self):
         """
         Render the screen.
         """
+
         arcade.start_render()
-        self.grid_shape_list.draw()
+        arcade.draw_rectangle_filled(7 * SCREEN_WIDTH // 16, 5 * SCREEN_HEIGHT // 9, FIELD_WIDTH, FIELD_HEIGHT,
+                                     arcade.color.AMAZON)
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
         Called when the user presses a mouse button.
         """
-        column = int(x // (MARGIN + WIDTH))
-        row = int(y // (MARGIN + HEIGHT))
-        if self.grid[row][column] == 0:
-            self.grid[row][column] = 1
-        else:
-            self.grid[row][column] = 0
-
-        self.create_shapes_from_grid()
+        pass
 
 
 def main():
 
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, 'Test')
     arcade.run()
 
 
